2076d779925bf6801a9e8c73d624d78440b53603
dinel <rddinel@gmail.com>
2025-12-09 17:26:20 +0100

    Correction 2 fs

--- Exercices/Ex2/2_fs.js
+++ Exercices/Ex2/2_fs.js
@@ -1,3 +1,24 @@
 // Lisez le contenu du fichier example.txt situé dans le dossier assets et affichez-le dans la console.
-// Créez un nouveau fichier appelé output.txt dans le même dossier et écrivez-y une chaîne de caractères : Ceci est un nouveau fichier créé avec Node.js
+// Créez un nouveau fichier appelé output.txt dans le même dossier et écrivez-y une chaîne de caractères : 
+// Ceci est un nouveau fichier créé avec Node.js
 // Supprimez le fichier output.txt que vous venez de créer.
+
+const fs = require('fs');
+
+// Lecture
+fs.readFile('assets/example.txt', 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+});
+
+// Écriture / Création 
+fs.writeFile('assets/output.txt', 'Ceci est un nouveau fichier créé avec Node.js', (err) => {
+    if (err) throw err;
+    console.log('Fichier créé avec succès!');
+});
+
+// Suppression
+fs.unlink('assets/output.txt', (err) => {
+    if (err) throw err;
+    console.log('Fichier supprimé avec succès!');
+});
